25fd722304ff23937a5a18904d2ac52f88ebfd6c
Daniel <danieldeandradelopes@gmail.com>
2022-01-12 13:25:34 -0300

    exercices

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const countRef = useRef(0);
+  const incrementRef = useRef(null);
+  const incrementLet = useRef(null);
+
   let count = 0;
 
   console.log("Renderizou");
   console.log(countRef.current, "countRef")
   console.log(count, "count")
 
-  function incrementCountRef() {
-    countRef.current += 1;
-    console.log("Valor de countRef", countRef.current);
-  }
+  useEffect(() => {
+    incrementRef.current.addEventListener('click', () => {
+      countRef.current += 1;
+      console.log("Valor de countRef", countRef.current);
+    });
 
-  function incrementCount() {
-    count += 1;
-    console.log("Valor de count", count);
-  }
+    incrementLet.current.addEventListener('click', () => {
+      count += 1;
+      console.log("Valor de count", count);
+    });
+
+  }, []);
 
   return (
     <div className="App">
-      <button onClick={() => incrementCountRef()}>Increment ref</button>
-      <button onClick={() => incrementCount()}>Increment let</button>
+      <button ref={incrementRef}>
+        Increment ref
+      </button>
+      <button ref={incrementLet}>
+        Increment let
+      </button>
     </div>
   );
 }
